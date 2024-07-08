d58cec2269daf859e473f5ae95e49b0861f93039
Vishu <vishuagarwal237@gmail.com>
2024-07-07 20:22:32 -0400

    No repeating in substring

--- Sliding Window/Permutation in String.py
+++ Sliding Window/Permutation in String.py
@@ -1,7 +1,11 @@
 '''
-1. Check count
+Idea is to check the Counter dict of each window of s2 to the Counter of s1
 
-
+1. Create a correct counter of the s1 string and then of the initial window of s2, do initial check to see if they are equal
+2. Loop through range of s2 untul len(s2)-len(s2)+1
+    add the new letter (s2[i+len(s1)-1]) to the dict, decrement the old letter(s2[i-1])
+    if the two counters match, return True
+3. Return false as default
 
 '''
 
