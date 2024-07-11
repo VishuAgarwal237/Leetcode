d42eb847e23ad14ef79b2db95da33f480dd735d5
Vishu <vishuagarwal237@gmail.com>
2024-07-10 22:04:46 -0400

    Trees

---
+++ Array & Hashing/Second Max.py
@@ -0,0 +1,16 @@
+
+def SecondMax(nums):
+    max = nums[0]
+    secondmax = float('-inf')
+    
+    for x in nums[1:]:
+        if x > max:
+            secondmax = max
+            max = x
+        elif x > secondmax:
+            secondmax = x
+    print(max)
+    print(secondmax)
+
+if __name__ == "__main__":
+    SecondMax([5,3,2,4,6,2,9,9])
